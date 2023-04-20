6754003c3f0a988290d80b6d1d0f5acb6edbbc72
nm <namcneal@umich.edu>
2023-04-20 02:02:07 -0400

    Starting to work on higher order derivatives. The jacobian function seems to be working on all monomials, polynomials, and rational functions that Ive tested. However, the tests at this commit are failing when I try to compute the second derivative of the outer product function in the main function of the test folder.

--- tests/main.rs
+++ tests/main.rs
@@ -1,36 +1,37 @@
-mod epsilon_polynomials;
+// mod epsilon_polynomials;
 
-mod jacobian_testing;
+// mod jacobian_testing;
 
 // mod higher_order_derivatives;
 
 
-// use epsilon_ad::prelude::*;
-// use epsilon_ad::differentiation::*;
+use epsilon_ad::prelude::*;
+use epsilon_ad::differentiation::*;
 
-// fn Ef(x:&EVector<f64>) -> EMatrix<f64>{
-//     let d = x.len();
-//     let mut outer = ndarray::Array2::from_elem(ndarray::Dim([d,d]), Dual::<f64>::zero());
+fn Ef(x:&EVector<f64>) -> EMatrix<f64>{
+    let d = x.len();
+    let mut outer = ndarray::Array2::from_elem(ndarray::Dim([d,d]), Dual::<f64>::zero());
     
-//     for a in 0..d{
-//         for b in 0..d{
-//             outer[[a,b]] = &x[a] * &x[b];
-//         }
-//     }
+    for a in 0..d{
+        for b in 0..d{
+            outer[[a,b]] = &x[a] * &x[b];
+        }
+    }
 
-//     return EArray(outer)
-// }
+    return EArray(outer)
+}
 
-// use ndarray::{s};
+use ndarray::{s};
 
-// fn main(){
-//     const dim : usize = 3;
-//     const order : usize = 1;
-//     let x0 = (ndarray::arr1(&[3.0, 5.0,7.0])).lift();
-//     let derivative_call = DerivativeInvocation::<f64,dim,order>::new(x0);
+#[test]
+fn main(){
+    const dim : usize = 3;
+    const order : usize = 2;
+    let x0 = ndarray::arr1(&[3.0, 5.0,7.0]);
+    let derivative_call = DerivativeInvocation::<f64,order>::new(x0);
 
-//     let output = derivative_call.tagged_eval(&Ef);
+    let output = derivative_call.tagged_eval(&Ef);
     
-//     println!("{:?}", output.extract_all_derivatives()[0].slice(s![..,..,1]));
-//     todo!()
-// }
+    println!("{:?}", output.extract_all_derivatives());
+    // todo!()
+}
